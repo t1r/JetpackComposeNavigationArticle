892beab3c2fd7c163eb592794290bdc8985edc34
t1r <>
2023-04-27 16:10:48 +0300

    step_cases

--- app/src/main/java/dev/jcnavigation/article/ui/express/cart/ExpressCartScreen.kt
+++ app/src/main/java/dev/jcnavigation/article/ui/express/cart/ExpressCartScreen.kt
@@ -1,9 +1,10 @@
 package dev.jcnavigation.article.ui.express.cart
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -11,10 +12,12 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ExpressCartScreen(
@@ -38,24 +41,31 @@ fun ExpressCartScreen(
                         onClick = onBackAction,
                     )
                 },
+                actions = {
+                    IconButton(
+                        content = {
+                            Icon(Icons.Filled.Home, "Home")
+                        },
+                        onClick = goToHome,
+                    )
+                },
             )
         },
         content = { pv ->
-            Box(
+            Column(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(pv),
+                    .padding(pv)
+                    .fillMaxSize(),
             ) {
-                Button(
+                Spacer(modifier = Modifier.weight(1F))
+                Text(
                     modifier = Modifier
-                        .align(Alignment.Center)
-                        .padding(vertical = 12.dp),
-                    onClick = goToHome,
-                ) {
-                    Text(
-                        text = "Go To Home",
-                    )
-                }
+                        .align(Alignment.CenterHorizontally)
+                        .padding(horizontal = 24.dp),
+                    text = "Express Cart",
+                    fontSize = 30.sp,
+                )
+                Spacer(modifier = Modifier.weight(1F))
             }
         }
     )
